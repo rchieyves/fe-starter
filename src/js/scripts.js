57633f633d0635e4b85ad9f564025e112af349c4
Christian Binay-an <99239543+cbins13@users.noreply.github.com>
2022-02-10 18:55:02 +0800

    update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,11 @@
 import module1 from "./modules/module-1";
 import module2 from "./modules/module-2";
-<<<<<<< Updated upstream
-
+import module3 from "./modules/module-cbins13";
 function init() {
   module1();
   module2();
 }
 
 init();
-=======
-import module3 from "./modules/module-cbins13";
+
 module1();
->>>>>>> Stashed changes
