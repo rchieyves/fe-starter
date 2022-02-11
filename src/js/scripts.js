623a54aebf399d03a299110137fb9c74d0a5c171
Jerome Bambao <2193871@slu.edu.ph>
2022-02-11 17:59:29 +0800

    Added scripts for dynamic viewing of the statistics

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@
 fetch("/api/dashboard")
   .then((response) => response.json())
   .then((data) => {
-    // 2. Render the Data
-    console.log(data);
+    // document.getElementById('total-value').innerHTML = data.primary_metrics.total.value;
+    // document.getElementById('total-value').innerHTML = 2000;
+    console.log(data.primary_metrics.total.value);
   });
