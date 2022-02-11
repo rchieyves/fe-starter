45cdd4c3775d8f289ca3751105ce8a0fb8fbad21
Jhorvs <2190101@slu.edu.ph>
2022-02-11 23:45:46 +0800

    added a gradient color on top border of ig css

--- src/js/modules/server.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");
-const server = express();
-const PORT = 3000;
-
-server.use(express.static("publick"));
-server.listen(PORT, function*(){
-    console.log('Server is now listening to port ${PORT}');
-
-})
