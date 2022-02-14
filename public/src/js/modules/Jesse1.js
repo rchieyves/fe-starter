8bec7c68a4123829251498ca2898c6fc3f7f861d
Jesse Odsey <jfodsey@gmail.com>
2022-02-14 16:42:52 +0800

    revised script to make the website generate cards from scripts

--- public/src/js/modules/Jesse1.js
+++ public/src/js/modules/Jesse1.js
@@ -1,44 +1,125 @@
-function add_trend(data){
-    var elements = document.getElementsByClassName("rating");
-    var list = list_trends(data);
-    for (var i = 0, len = elements.length; i < len; i++) {
+function generate_cards(data){
+    generate_card_big(data);
+    generate_card_small(data);
+}
 
+function generate_card_big(data){
+    var data_big_cards = data["primary_metrics"]["cards"];
+    for (var i = 0, len = data_big_cards.length; i < len; i++) {
+        var tempdata = data_big_cards[i];
+
+        /* div big cards*/
+        const card = document.createElement("div");
+        card.classList.add("card", "card_big", tempdata["service"].toString() );
+        document.getElementsByTagName("BODY")[0].insertBefore(card, document.getElementById("overview"));
+        
+
+        /* trend images */
         const up = document.createElement("img");
         up.src = "./src/images/icon-up.svg";
-        up.style.width="7px";
-        up.style.height="5px";
-        up.style.marginRight="2px";
-        up.style.marginBottom="2px";
-
+        up.classList.add("trend_image");
         const down = document.createElement("img");
         down.src = "./src/images/icon-down.svg";
-        down.style.width="7px";
-        down.style.height="5px";
-        down.style.marginRight="2px";
-        up.style.marginBottom="2px";
+        down.classList.add("trend_image")
 
-        var tempElement = document.getElementsByClassName("rating")[i];
+        /* name */
+        var name = document.createElement("span");
+        var logo = document.createElement("img");
+        name.appendChild(logo);
+        if(tempdata["service"]=="youtube"){
+            name.append(tempdata["username"]);
+        }else{
+            name.append("@"+tempdata["username"]);
+        }
+        name.classList.add("grey", "small");
+        logo.src="./src/images/icon-"+tempdata["service"]+".svg";
+        logo.classList.add("logo");
+        card.appendChild(name);
 
-        if(list[i]["trend"]=="up"){
-            tempElement.classList.add("green");
-            tempElement.insertBefore(up,tempElement.firstChild);
+        /* number */
+        var number = document.createElement("h1");
+        number.append(tempdata["value"]);
+        card.appendChild(number);
+
+        /* followers */
+        var followers = document.createElement("p");
+        followers.append(tempdata["label"]);
+        followers.classList.add("grey", "card_followers");
+        card.appendChild(followers);
+
+        /* rating */
+        var rating = document.createElement("p");
+        rating.append(tempdata["metric"]["value"]+" Today");
+        if(tempdata["metric"]["trend"]=="up"){
+            rating.classList.add("green");
+            rating.insertBefore(up,rating.firstChild);
         }
-        else if(list[i]["trend"]=="down"){
-            tempElement.classList.add("red");
-            tempElement.insertBefore(down,tempElement.firstChild);
+        else if(tempdata["metric"]["trend"]=="down"){
+            rating.classList.add("red");
+            rating.insertBefore(down,rating.firstChild);
         }
+        rating.classList.add("rating");
+        card.appendChild(rating);
     }
 }
 
-function list_trends(data){
-    var list = [];
-    for(var i=0; i<data["primary_metrics"]["cards"].length; i++){
-        list.push(data["primary_metrics"]["cards"][i]["metric"]);
+function generate_card_small(data){
+    var data_small_cards = data["supporting_metrics"];
+    for (var i = 0, len = data_small_cards.length; i < len; len--) {
+        var tempdata = data_small_cards[len-1];
+
+        /* div small cards*/
+        const card = document.createElement("div");
+        card.classList.add("card", "card_small");
+        document.getElementById("overview").parentNode.insertBefore(card,document.getElementById("overview").nextSibling);
+        
+
+        /* trend images */
+        const up = document.createElement("img");
+        up.src = "./src/images/icon-up.svg";
+        up.classList.add("trend_image");
+        const down = document.createElement("img");
+        down.src = "./src/images/icon-down.svg";
+        down.classList.add("trend_image")
+
+        /* Views or Likes */
+        var views = document.createElement("span");
+        views.classList.add("left", "grey", "sub");
+        views.append(tempdata["label"]);
+        card.appendChild(views)
+
+        /* logo */
+        var img_span = document.createElement("span");
+        var logo = document.createElement("img");
+        img_span.appendChild(logo);
+        img_span.classList.add("right");
+        logo.src="./src/images/icon-"+tempdata["service"]+".svg";
+        card.appendChild(img_span);
+
+        /*breaks*/
+        card.appendChild(document.createElement("br"));
+        card.appendChild(document.createElement("br"));
+
+        /* number */
+        var number = document.createElement("span");
+        number.append(tempdata["value"]);
+        number.classList.add("left","card_small_number")
+        card.appendChild(number);
+
+        /* rating */
+        var rating = document.createElement("span");
+        rating.append(tempdata["metric"]["percent"]+"%");
+        if(tempdata["metric"]["trend"]=="up"){
+            rating.classList.add("green");
+            rating.insertBefore(up,rating.firstChild);
+        }
+        else if(tempdata["metric"]["trend"]=="down"){
+            rating.classList.add("red");
+            rating.insertBefore(down,rating.firstChild);
+        }
+        rating.classList.add("rating","right");
+        card.appendChild(rating);
     }
-    for(var i=0; i<data["supporting_metrics"].length; i++){
-        list.push(data["supporting_metrics"][i]["metric"]);
-    }
-    return list;
 }
 
-export default add_trend;
+export default generate_cards;
--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -1,13 +1,13 @@
 import module1 from "./modules/module-1";
 import module2 from "./modules/module-2";
 import get_json from "./modules/Jesse";
-import add_trend from "./modules/Jesse1";
+import generate_cards from "./modules/Jesse1";
 
 function init() {
   module1();
   module2();
   get_json().then((data)=>
-  add_trend(data))
+  generate_cards(data))
 }
 
 init();
