d7c7103336c927729fbcb94ae9d2476e5bce5992
Christian Binay-an <christianbinayan@gmail.com>
2022-02-16 05:02:08 +0800

    modified public index, css, added dist folder for resources

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const server = express();
 const PORT = 3000;
 
 server.use(express.static("public"));
-
 server.use("/api/dashboard", function (req, res) {
   const dashboardData = JSON.parse(fs.readFileSync("data.json"));
   res.send(dashboardData).status(200).end();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import module1 from "./modules/module-1";
-import module2 from "./modules/module-2";
-import module3 from "./modules/module-cbins13";
-function init() {
-  module1();
-  module2();
-}
-
-init();
-
-module1();
-=======
 // 1. Fetch Data
 fetch("/api/dashboard")
   .then((response) => response.json())
@@ -18,4 +5,3 @@ fetch("/api/dashboard")
     // 2. Render the Data
     console.log(data);
   });
->>>>>>> origin/feature/joshuacerbito/server-setup
