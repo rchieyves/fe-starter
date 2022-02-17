760c5ee860892c0aa25c4985e838df4cb4a3ca1f
AlvinRealtalk <alvnonline@yahoo.com>
2022-02-17 15:40:02 +0800

    - Changed to dynamic port - Added Procfile

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const express = require("express");
 const server = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 
 server.use(express.static("public"));
 
